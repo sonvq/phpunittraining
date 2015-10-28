a54cf31b860eaa353e201e40bfe4fcdc58f97796
Steve <sonvq@qsoft.com.vn>
2015-10-28 14:54:07 +0700

    commit

--- demo/visualphpunitui/app/testphpunit/Double/Basic/MockMethodTest.php
+++ demo/visualphpunitui/app/testphpunit/Double/Basic/MockMethodTest.php
@@ -13,24 +13,4 @@ class MockMethodTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals('bar', $baseDouble->doSomething());
     }
 
-    public function testMockMethodTemp() {
-        $baseDouble = $this->getMockBuilder('BaseDouble')
-            ->setMethods(array('doSomething'))
-            ->getMock();
-
-        $baseDouble->expects($this->once())
-            ->method('doSomething')
-            ->will($this->returnValue('barss'));
-
-        $baseDouble->doSomething();
-    }
-
-    public function testStubMethod() {
-        $baseDouble = $this->getMockBuilder('BaseDouble')
-            ->getMock();
-        var_dump($baseDouble->doSomething());
-
-        $this->assertEquals('bar', $baseDouble->doSomething());
-    }
-
 }
--- demo/visualphpunitui/app/testphpunit/Double/Basic/StubMethodTest.php
+++ demo/visualphpunitui/app/testphpunit/Double/Basic/StubMethodTest.php
@@ -24,5 +24,32 @@ class StubMethodTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals('foo', $baseDouble->doSomething());
         
     }
+    
+    public function testStubMethodAdvance() {
+        $baseDouble = $this->getMockBuilder('BaseDouble')
+            ->setMethods(array('doSomething'))
+            ->getMock();
+
+//        $baseDouble->expects($this->once())
+//            ->method('doSomething')
+//            ->will($this->returnValue('barss'));
+        
+        $baseDouble->expects($this->exactly(2))
+            ->method('doSomething')
+            ->will($this->returnValue('barss'));
+
+        $baseDouble->doSomething();
+        $baseDouble->doSomething();
+        $baseDouble->doSomething();
+        
+        // Configure the stub.
+        $stub->method('doSomething')
+             ->will($this->onConsecutiveCalls(2, 3, 5, 7));
+
+        // $stub->doSomething() returns a different value each time
+        $this->assertEquals(2, $stub->doSomething());
+        $this->assertEquals(3, $stub->doSomething());
+        $this->assertEquals(5, $stub->doSomething());
+    }
 
 }
